3255f335511190653ca4fd0241b369f9088ca68c
Sam Lin <greenlin111@gmail.com>
2017-10-01 02:49:35 +0800

    add authenticate_admin method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def authenticate_admin
+    raise ActiveRecord::RecordNotFound unless current_user.admin?
+  end
 end
