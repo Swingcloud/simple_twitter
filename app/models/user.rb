c205d1b09a64675481eb10bcb430133c975534ed
Sam Lin <greenlin111@gmail.com>
2017-09-30 21:01:36 +0800

    model:User add admin? and tag

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def admin?
+    self.role == "admin"
+  end
+
+  def tag
+    "@#{self.tag_name}"
+  end
 end
