c59acba3a60b7fd5d84a86a880155cb37aebdf43
Sam Lin <greenlin111@gmail.com>
2017-10-06 01:07:10 +0800

    add relation: Friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,12 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'user_id'
   has_many :like_tweets, through: :likes, source: :tweet
 
+  has_many :following_friendships, class_name: 'Friendship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :following, through: :following_friendships, source: :followed
+
+  has_many :followed_friendships, class_name: 'Friendship', foreign_key: 'followed_id', dependent: :destroy
+  has_many :followers, through: :followed_friendships, source: :follower
+
   def admin?
     self.role == "admin"
   end
