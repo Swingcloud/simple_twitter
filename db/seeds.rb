5b1405c7431034d36389ee14ef8a13d2ab55ea8e
Sam Lin <greenlin111@gmail.com>
2017-10-05 15:25:06 +0800

    add db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# create one admin user
+User.create!(email: 'twitter_admin@alphacamp.com', password: '12345678', tag_name: 'mighty_admin', role: 'admin')
+
+10.times do
+  user = User.create!(email: Faker::Internet.unique.email,
+                      password: '12345678',
+                      tag_name: Faker::DrWho.unique.specie.gsub(/\s+/, '')
+                     )
+  puts "#{user.email} is being created!"
+
+  5.times do |i|
+    user.tweets.create!( description: Faker::Lorem.sentence,
+                         user_id: user.id
+                       )
+    puts "tweet#{i+1} is being created!"
+  end
+end
