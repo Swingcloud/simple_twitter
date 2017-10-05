21c5207b707b27c779b8bd2f98f3e6f77c439e11
Sam Lin <greenlin111@gmail.com>
2017-10-05 15:08:30 +0800

    create model: Tweet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170930123859) do
+ActiveRecord::Schema.define(version: 20171005065734) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.text "description", default: "", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["id", "user_id"], name: "index_tweets_on_id_and_user_id", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
